1f5609ed2f75dde36cc3e7629b9f4d4f7f5ae4c2
tihorsharma1306 <tihorsharma123@gmail.com>
2026-07-19 19:24:01 +0530

    Update A - Zero Sum.cpp

--- Codeforces Round 1111 (Div. 2)/A - Zero Sum.cpp
+++ Codeforces Round 1111 (Div. 2)/A - Zero Sum.cpp
@@ -1,1 +1,25 @@
+#include "bits/stdc++.h"
 
+using namespace std;
+
+int n;
+
+int main() {
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        cin >> n;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            int x;
+            cin >> x;
+            sum += x;
+        }
+        if (abs(sum) % 4 == 0) {
+            cout << "YES" << "\n";
+        } else {
+            cout << "NO" << "\n";
+        }
+    }
+    return 0;
+}
